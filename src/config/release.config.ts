d76e40f1051b981c0a00d5443b39d2c8fabdf5e3
Dawit <dawitabate@live.it>
2026-09-10 22:19:09 +0200

    feat(release): update version to 0.13.0-rc.2 and add release notes for new features

--- src/config/release.config.ts
+++ src/config/release.config.ts
@@ -24,11 +24,11 @@ export interface ReleaseManifest {
 }
 
 export const RELEASE: ReleaseManifest = {
-  version: '0.13.0-rc.1',
-  bootstrapRef: '8bdbb6f',
+  version: '0.13.0-rc.2',
+  bootstrapRef: '4957101',
   images: {
-    fluiApi: '0.13.0-rc.1',
-    fluiWeb: '0.13.0-rc.1',
+    fluiApi: '0.13.0-rc.2',
+    fluiWeb: '0.13.0-rc.2',
     fluiAuthz: '0.6.0',
   },
 };
